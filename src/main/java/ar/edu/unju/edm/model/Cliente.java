209782b601c444d60a4a961a3bf7b683916617d1
Lucas Argota <lucas.argota287@gmail.com>
2021-07-05 21:05:07 -0300

    agregado de cascade en cliente

--- src/main/java/ar/edu/unju/edm/model/Cliente.java
+++ src/main/java/ar/edu/unju/edm/model/Cliente.java
@@ -1,13 +1,17 @@
 package ar.edu.unju.edm.model;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
@@ -69,7 +73,8 @@ public class Cliente {
 	
 
 	
-	
+	@OneToMany(mappedBy ="cliente", cascade = CascadeType.ALL)
+	private List<Venta> ventas = new ArrayList<Venta>();
 	
 	
 	
